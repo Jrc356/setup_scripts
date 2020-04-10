6af3ae631b0eea36422d0d9008913ceef05fdefa
Jon Corbin <jon.corbin8@gmail.com>
2020-04-10 00:16:34 -0400

    move zsh command to last

--- manjaro/setup.sh
+++ manjaro/setup.sh
@@ -74,7 +74,7 @@ if [ ! -d "${ZSH}/custom/themes/powerlevel10k" ] ; then
 	git clone https://github.com/romkatv/powerlevel10k.git ${ZSH}/custom/themes/powerlevel10k
 fi
 
-zsh
-
 git config --global user.email "jon.corbin8@gmail.com"
 git config --global user.name "Jon Corbin"
+
+zsh
